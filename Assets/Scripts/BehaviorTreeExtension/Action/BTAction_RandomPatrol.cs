586211ebb355dd84bfbf0147955bc5c67bb0c93a
LiuDingxin <2230457162@qq.com>
2025-05-13 23:15:59 +0800

    更新：铅笔怪只会吐（铅笔没复现，疑似配置问题）；敌人走路平移；铁头会呆住

--- Assets/Scripts/BehaviorTreeExtension/Action/BTAction_RandomPatrol.cs
+++ Assets/Scripts/BehaviorTreeExtension/Action/BTAction_RandomPatrol.cs
@@ -12,6 +12,8 @@ namespace BehaviorTreeExtension
         private AIPath aiPath;
         private Animator animator;
         
+        private static bool firstRun = true;
+        private static Vector3 lastMoveDirection; 
         public override void OnAwake()
         {
             base.OnAwake();
@@ -23,9 +25,38 @@ namespace BehaviorTreeExtension
         public override void OnStart()
         {
             base.OnStart();
-            
-            aiPath.destination = Random.insideUnitCircle*PatrolRange;
-            
+            if (firstRun)
+            {
+                float r = Random.Range(0,1);
+                aiPath.destination = Random.insideUnitCircle*(r*PatrolRange);
+                lastMoveDirection = (aiPath.destination - entity.transform.position).normalized;
+                firstRun = false;
+            }
+            else
+            {
+                int type = IsInBoundGraphNode(entity.transform.position);
+                switch (type)
+                {
+                    case 0:
+                        aiPath.destination = lastMoveDirection*PatrolRange;
+                        break;
+                    case 1: //left
+                        lastMoveDirection.x *= -1;
+                        break;
+                    case 2: //right
+                        lastMoveDirection.x *= -1;
+                        break;
+                    case 3: //bottom
+                        lastMoveDirection.y *= -1;
+                        break;
+                    case 4: //top
+                        lastMoveDirection.y *= -1;
+                        break;
+                }
+                aiPath.destination = lastMoveDirection*PatrolRange;
+                lastMoveDirection = (aiPath.destination - entity.transform.position).normalized;
+            }
+            Debug.Log(lastMoveDirection);
             aiPath.canMove = true;
             animator.SetFloat("MoveSpeed",aiPath.maxSpeed);
         }
@@ -51,7 +82,36 @@ namespace BehaviorTreeExtension
         
             return TaskStatus.Running;
         }
-      
+
+        private int IsInBoundGraphNode(Vector3 position)
+        {
+            var nodeInfo = AstarPath.active.GetNearest(position);
+            var gridGraph = AstarPath.active.data.gridGraph;
+            uint nodeIndex = nodeInfo.node.NodeIndex;
+            uint nodeY = nodeIndex % (uint)gridGraph.width;
+            uint nodeX = (nodeIndex-nodeY) / (uint)gridGraph.width;
+            uint height = (uint)gridGraph.size.y;
+            
+            if ((nodeX <= 1 || nodeX >= gridGraph.width) || 
+                (nodeY <= 1 || nodeY >= height))
+            {
+                if (nodeX <= 1)
+                {
+                    return 1; //left
+                }
+                if (nodeX >= gridGraph.width)
+                {
+                    return 2;//right
+                }
+                if (nodeY <= 1)
+                {
+                    return 3;//bottom
+                }
+                return 4;//top
+            }
+            return 0;
+        }
+        
         public override void OnEnd()
         {
             aiPath.canMove = false;
