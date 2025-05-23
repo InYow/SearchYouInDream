d76753cda021d44c7434d185db7d1ac50bebea77
LiuDingxin <2230457162@qq.com>
2025-05-23 15:28:14 +0800

    添加阴影偏移

--- Assets/Derek/Scripts/Enemy/Enemy_BossBase.cs
+++ Assets/Derek/Scripts/Enemy/Enemy_BossBase.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy_BossBase : Enemy
 {
+    public ExternalBehaviorTree FirstStageBehaviorTree;
+    public ExternalBehaviorTree SecondStageBehaviorTree;
+
+    public override void Start()
+    {
+        base.Start();
+        SetBehaviorTree(FirstStageBehaviorTree);
+    }
     
+    public void EnterSecondStage()
+    {
+        SetBehaviorTree(SecondStageBehaviorTree);
+    }
+    
+    private void SetBehaviorTree(ExternalBehaviorTree treeAsset)
+    {
+        if (treeAsset)
+        {
+            behaviourTree.ExternalBehavior = treeAsset;    
+        }
+    }
 }
